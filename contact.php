93ce2cdc2d8ff0be46594d495119f2f6ee427bfd
HiraKamal <hirakamal98@gmail.com>
2018-11-19 21:39:06 +0500

    Updated contact.php
    SendMail commented.

--- contact.php
+++ contact.php
@@ -1,5 +1,28 @@
 <?php
+include './scripts/DbConnect.php';
 include 'header.php';
+
+
+if((isset($_POST['name'])&& $_POST['name'] !='') && (isset($_POST['email']) && $_POST['email'] != '')) {
+    $Name = $_POST['name'];
+    $EmailId = $_POST['email'];
+    $PhoneNo = $_POST['phone'];
+    $Message = $_POST['message'];
+
+
+    if (true) {
+
+        $ins = "INSERT INTO contact (Name,EmailId,PhoneNo,Message) VALUES ('$Name', '$EmailId' , '$PhoneNo', '$Message')";
+        /*echo "iam here";*/
+        if ($con->query($ins)) {
+            /*require_once("sendMail.php");*/
+            echo "<script>alert('Thank you we will contact you soon !!');location.href = 'contact.php'</script>";
+        }
+    } else {
+        echo "<script>alert('Please fill  name and email !');location.href = 'contact.php'</script>";
+    }
+
+}
 ?>
 
 <div class="google-maps">
@@ -13,36 +36,38 @@ include 'header.php';
             	<div class="row">
             		<div class="col-md-8 col-sm-7">
                   <h4 class="grey">Leave a Message</h4>
-                  <form class="contact-form">
+                  <form class="contact-form" method="post">
                     <div class="form-group">
                       <i class="icon ion-person"></i>
                       <input id="contact-name" type="text" name="name" class="form-control" placeholder="Enter your name *" required="required" data-error="Name is required.">
                     </div>
                     <div class="form-group">
                       <i class="icon ion-email"></i>
-                      <input id="contact-email" type="text" name="email" class="form-control" placeholder="Enter your email *" required="required" data-error="Email is required.">
+                      <input id="contact-email" type="email" name="email" class="form-control" placeholder="Enter your email *" required="required" data-error="Email is required.">
                     </div>
                     <div class="form-group">
                       <i class="icon ion-android-call"></i>
-                      <input id="contact-phone" type="text" name="phone" class="form-control" placeholder="Enter your phone *" required="required" data-error="Phone is required.">
+                      <input id="contact-phone" type="text" name="phone" class="form-control" placeholder="Enter your phone *">
                     </div>
                     <div class="form-group">
                       <textarea id="form-message" name="message" class="form-control" placeholder="Leave a message for us *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
                     </div>
+                      <button type="send" name="msgbtn" class="btn-primary" value="insert">Send a Message</button>
+                    </div>
                   </form>
-                  <button class="btn-primary">Send a Message</button>
-                </div>
+
+
             		<div class="col-md-4 col-sm-5">
                   <h4 class="grey">Reach Us</h4>
                   <div class="reach"><span class="phone-icon"><i class="icon ion-android-call"></i></span><p>+1 (234) 222 0754</p></div>
                   <div class="reach"><span class="phone-icon"><i class="icon ion-email"></i></span><p>info@thunder-team.com</p></div>
                   <div class="reach"><span class="phone-icon"><i class="icon ion-ios-location"></i></span><p>228 Park Ave S NY, USA</p></div>
                   <ul class="list-inline social-icons">
-                    <li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
-                    <li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
-                    <li><a href="#"><i class="icon ion-social-googleplus"></i></a></li>
-                    <li><a href="#"><i class="icon ion-social-pinterest"></i></a></li>
-                    <li><a href="#"><i class="icon ion-social-linkedin"></i></a></li>
+                    <li><a href="http://www.facebook.com"><i class="icon ion-social-facebook"></i></a></li>
+                    <li><a href="https://www.twitter.com"><i class="icon ion-social-twitter"></i></a></li>
+                    <li><a href="https://plus.google.com/discover"><i class="icon ion-social-googleplus"></i></a></li>
+                    <li><a href="https://www.pinterest.com"><i class="icon ion-social-pinterest"></i></a></li>
+                    <li><a href="https://pk.linkedin.com"><i class="icon ion-social-linkedin"></i></a></li>
                   </ul>
                 </div>
             	</div>
