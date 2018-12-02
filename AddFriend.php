347eb80e231550ea3652a4dfe54df3a29025fd78
HiraKamal <hirakamal98@gmail.com>
2018-12-02 21:12:35 +0500

    Add Friend done

--- AddFriend.php
+++ AddFriend.php
@@ -1,46 +1,40 @@
 <?php
-include '../header.php';
+//include '../header.php';
 include 'DbConnect.php';
 session_start();
 
 if(!isset($_SESSION['kuchBhi']))
 {
-    echo "<script>alert('Login Required !');location.href = 'index-register.php'</script>";
+    echo "<script>alert('Login Required !');location.href = '../index-register.php'</script>";
 }
 else{
+
     $email = $_SESSION['kuchBhi'];
-    //echo "$email is Logged In<br>";
     $sel = "SELECT * FROM users WHERE email = '$email'";
     $ex_sel = $con->query($sel);
     $data = mysqli_fetch_array($ex_sel);
     $userid=$data['id'];
-
-    $gett="SELECT * FROM users WHERE id!='$userid' ";
-    $query=mysqli_query($con,$gett) or die('error');
-    while($row=mysqli_fetch_array($query,MYSQLI_ASSOC)) {
-        $Recname = $row['fname']. " " . $row['lname'];
-        $receiverId=$row['id'];
-    }
-
+    echo " mmy id :$userid";
+    $recId=$_GET['Recid'];
+    echo "recId: $recId";
 
     if(isset($_POST['addbtn']))
     {
-        $query= "INSERT INTO friendrequest(sender_id,receiver_id) VALUES ('$userid','$receiver_id')";
-        $obj=new User($con,$id);
+        echo "ma agyi";
+        $query= "INSERT INTO friendrequest(sender_id,receiver_id) VALUES ('$userid','$recId')";
+//        $obj=new User($con,$id);
 
 
         if ($con->query($query)) {
-            echo "<script>alert('Following/Added to friends');location.href = 'newsfeed.php'</script>";
+            echo "<script>alert('Following/Added to friends');location.href = '../newsfeed.php'</script>";
         }
         else{
             echo "ni chla";
         }
 
     }
-
-
-
 }
+
 
 
 ?>
