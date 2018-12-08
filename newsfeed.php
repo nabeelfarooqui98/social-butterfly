d05f18fb4f1d21bd39d70086a1686d716e1a1571
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2018-12-08 23:56:49 +0500

    newsfeed
    * removed commented code
    * clicking on posters name takes you to their timeline instad of timeline.php for all
    * added space in name of poster

--- newsfeed.php
+++ newsfeed.php
@@ -11,7 +11,7 @@ $que1 = "SELECT * FROM users WHERE email='$email'";
 $ex_que1 = $con->query($que1);
 $userse = mysqli_fetch_array($ex_que1);
 $id = $userse['id'];
-$name = $userse['fname'] . $userse['lname'];
+$name = $userse['fname'] . " " . $userse['lname'];
 ?>
 
     <div id="page-contents">
@@ -38,8 +38,6 @@ $name = $userse['fname'] . $userse['lname'];
                     <ul class="publishing-tools list-inline">
                       <li><a href="#"><i class="ion-compose"></i></a></li>
                       <li><a href="newsfeedimage.php" onclick><i class="ion-images"></i></a></li>
-                      <li><a href="#"><i class="ion-ios-videocam"></i></a></li>
-                      <li><a href="#"><i class="ion-map"></i></a></li>
                     </ul>
                     <button type="submit" id="pubbtn" name="pubbtn" class="btn btn-primary pull-right">Publish</button>
                   </div>
@@ -54,7 +52,7 @@ $name = $userse['fname'] . $userse['lname'];
             ================================================= -->
 
           <?php
-
+//could have used join?
               $gett="SELECT * FROM posts ORDER BY timestamp DESC ";
               $query=mysqli_query($con,$gett) or die('error');
               while($row=mysqli_fetch_array($query,MYSQLI_ASSOC)){
@@ -62,7 +60,7 @@ $name = $userse['fname'] . $userse['lname'];
                 $que = "SELECT * FROM users WHERE id='$userid'";
                 $ex_que = $con->query($que);
                 $use = mysqli_fetch_array($ex_que);
-                $name = $use['fname'] . $use['lname'];
+                $name = $use['fname'] . " ". $use['lname'];
               ?>
               <div class="post-content">
                 <?php
@@ -75,7 +73,7 @@ $name = $userse['fname'] . $userse['lname'];
 
                     <div class="post-detail">
                       <div class="user-info">
-                        <h5><a href="timeline.php" class="profile-link"> <?php echo $name; ?> </a> <span
+                        <h5><a href="friendtl.php?uid=<?php echo $userid; ?>" class="profile-link"> <?php echo $name; ?> </a> <span
                               class="following">following</span></h5>
 
                         <p class="text-muted">Published a photo</p>
@@ -119,7 +117,7 @@ $name = $userse['fname'] . $userse['lname'];
 
                   <div class="post-detail">
                     <div class="user-info">
-                      <h5><a href="timeline.php" class="profile-link"> <?php echo $name; ?> </a> <span
+                      <h5><a href="friendtl.php?uid=<?php echo $userid; ?>" class="profile-link"> <?php echo $name; ?> </a> <span
                             class="following">following</span></h5>
 
                       <p class="text-muted">Published a post</p>
@@ -134,21 +132,7 @@ $name = $userse['fname'] . $userse['lname'];
                     </div>
 
                     <div class="line-divider"></div>
-                    <?php /*
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <img src="images/prof/<?php echo $image; ?>" alt="" class="profile-photo-sm">
-                    <p><a href="timeline.php" class="profile-link">Diana </a><i class="em em-laughing"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">John</a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <input type="text" class="form-control" placeholder="Post a comment">
-                  </div>
- */
+                    <?php 
                     }
                   ?>
                     </div>
@@ -156,151 +140,6 @@ $name = $userse['fname'] . $userse['lname'];
         <?php 
           }
           ?>
-                    <?php
-                    /*
-                    ?>
-            <!-- Post Content
-            ================================================= -->
-            <div class="post-content">
-               <video class="post-video" controls> <source src="videos/1.mp4" type="video/mp4"> </video>
-              <div class="post-container">
-                <img src="http://placehold.it/300x300" alt="user" class="profile-photo-md pull-left" />
-                <div class="post-detail">
-                  <div class="user-info">
-                    <h5><a href="timeline.php" class="profile-link">Sophia Lee</a> <span class="following">following</span></h5>
-                    <p class="text-muted">Updated her status about 33 mins ago</p>
-                  </div>
-                  <div class="reaction">
-                    <a class="btn text-green"><i class="icon ion-thumbsup"></i> 75</a>
-                    <a class="btn text-red"><i class="fa fa-thumbs-down"></i> 8</a>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                  </div>
-                  <div class="line-divider"></div>
-                   <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Olivia </a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <i class="em em-anguished"></i> Ut enim ad minim veniam, quis nostrud </p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Sarah</a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Linda</a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <input type="text" class="form-control" placeholder="Post a comment">
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Post Content
-            ================================================= -->
-            <div class="post-content">
-              <div class="post-container">
-                <img src="http://placehold.it/300x300" alt="user" class="profile-photo-md pull-left" />
-                <div class="post-detail">
-                  <div class="user-info">
-                    <h5><a href="timeline.php" class="profile-link">Linda Lohan</a> <span class="following">following</span></h5>
-                    <p class="text-muted">Published a photo about 1 hour ago</p>
-                  </div>
-                  <div class="reaction">
-                    <a class="btn text-green"><i class="icon ion-thumbsup"></i> 23</a>
-                    <a class="btn text-red"><i class="fa fa-thumbs-down"></i> 4</a>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-text">
-                    <p><i class="em em-thumbsup"></i> <i class="em em-thumbsup"></i> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Cris </a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam <i class="em em-muscle"></i></p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <input type="text" class="form-control" placeholder="Post a comment">
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Post Content
-            ================================================= -->
-            <div class="post-content">
-              <img src="http://placehold.it/2000x1300" alt="post-image" class="img-responsive post-image" />
-              <div class="post-container">
-                <img src="http://placehold.it/300x300" alt="user" class="profile-photo-md pull-left" />
-                <div class="post-detail">
-                  <div class="user-info">
-                    <h5><a href="timeline.php" class="profile-link">John Doe</a> <span class="following">following</span></h5>
-                    <p class="text-muted">Published a photo about 2 hour ago</p>
-                  </div>
-                  <div class="reaction">
-                    <a class="btn text-green"><i class="icon ion-thumbsup"></i> 39</a>
-                    <a class="btn text-red"><i class="fa fa-thumbs-down"></i> 2</a>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-text">
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt</p>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Brian </a>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Richard</a> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <input type="text" class="form-control" placeholder="Post a comment">
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Post Content
-            ================================================= -->
-            <div class="post-content">
-              <img src="http://placehold.it/1920x1160" alt="" class="img-responsive post-image" />
-              <div class="post-container">
-                <img src="http://placehold.it/300x300" alt="user" class="profile-photo-md pull-left" />
-                <div class="post-detail">
-                  <div class="user-info">
-                    <h5><a href="timeline.php" class="profile-link">Anna Young</a> <span class="following">following</span></h5>
-                    <p class="text-muted">Published a photo about 4 hour ago</p>
-                  </div>
-                  <div class="reaction">
-                    <a class="btn text-green"><i class="icon ion-thumbsup"></i> 2</a>
-                    <a class="btn text-red"><i class="fa fa-thumbs-down"></i> 0</a>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-text">
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.</p>
-                  </div>
-                  <div class="line-divider"></div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <p><a href="timeline.php" class="profile-link">Julia </a>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-                  </div>
-                  <div class="post-comment">
-                    <img src="http://placehold.it/300x300" alt="" class="profile-photo-sm" />
-                    <input type="text" class="form-control" placeholder="Post a comment">
-                  </div>
-                </div>
-              </div>
-            </div>
-                    <?php
-                    */
-                    ?>
-
 
     	</div>
               </div>
